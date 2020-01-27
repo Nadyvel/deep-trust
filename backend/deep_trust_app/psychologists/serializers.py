2d379e296e0781309bb9de76ed1530473bf9e8e2
Nadja Gujer <velnady@gmail.com>
2020-01-27 16:29:45 +0100

    Psychologists serializers.py fixed linting bug

--- backend/deep_trust_app/psychologists/serializers.py
+++ backend/deep_trust_app/psychologists/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 # from users.serializers import UserSerializer
+from deep_trust_app import psychologists
 from deep_trust_app.psychologists.models import Psychologist
 
 
@@ -8,7 +9,7 @@ class PsychologistSerializer (serializers.ModelSerializer):
     total_ratings = serializers.SerializerMethodField()  # searches for a function which is called get_total_ratings and executes it
 
     def get_total_ratings(self, restaurant):
-        return psychologist.reviews.count()   # gives all the relations and the amount of ratings for the psychologist
+        return psychologists.reviews.count()   # gives all the relations and the amount of ratings for the psychologist
 
     class Meta:
         model = Psychologist
--- backend/deep_trust_app/psychologists/views.py
+++ backend/deep_trust_app/psychologists/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth import get_user_model
 from django.db.models import Q
 
 
-
 # Create your views here.
 
 from rest_framework import status
