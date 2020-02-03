065d337d5aeba70311e525a4b99a75348aa6b6cf
Jessica Gibbons <jessica@Jessicas-MacBook-Air.local>
2020-02-03 21:44:45 +0100

    toogle side bar

--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -3,22 +3,40 @@ import {connect} from "react-redux";
 import {psychologistsAction} from '../../store/action/psychologistsAction';
 import './LandingPage.css';
 import {useEffect} from "react"
-import RegistrationModal from '../RegistrationModal/RegistrationModal'
-import {setModal} from "../../store/action/modalAction";
-import LoginModal from '../LoginModal/LoginModal';
+// import RegistrationModal from '../RegistrationModal/RegistrationModal'
+// import {setModal} from "../../store/action/modalAction";
+// import LoginModal from '../LoginModal/LoginModal';
 import Psychologists from '../Psychologists/Psychologists'
 
 const LandingPage = (props) => {
+    let [showSidebar, setShowSidebar] = useState(false);
+
     useEffect(() => {
         props.dispatch(psychologistsAction())
     }, []);
 
+    const handleSidebar = (e) => {
+        e.preventDefault();
+        setShowSidebar(!showSidebar)
+    };
+
+    let sideBarClassName = ''
+    if (showSidebar){
+        sideBarClassName = 'sidebarBox sidebarVisible'
+    } else {
+        sideBarClassName = 'sidebarBox'
+    }
+
     return (
         <div className='landingPage'>
 
-          <p className='line'></p>
+            <div className={sideBarClassName}>{showSidebar}</div>            
+
+            <p className='line'></p>
             <div className='parallax1'>
+                
                 <div className="layer">
+                <button onClick={(e) => handleSidebar(e)}>Display sidebar</button>
                     <p id="text">
                         DEEP TRUST
                     </p>
