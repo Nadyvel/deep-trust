5418cc98d30369df3fa41285fe95c2daf54d2be3
hmanaog <hmanaog@gmail.com>
2020-02-03 10:46:59 +0100

    - Modified Email format. - Added ImageField in User model - Modified Psychologist ImageField verbose name - Modified views in psychologist. - New migrations

--- backend/deep_trust_app/psychologists/urls.py
+++ backend/deep_trust_app/psychologists/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from deep_trust_app.psychologists.views import ListCreatePsychologistProfile, SearchPsychologists, \
-    UpdateDestroyPsychologist, UpdatePsychologistProfile
+from deep_trust_app.psychologists.views import CreatePsychologistProfile, SearchPsychologists, \
+    UpdateDestroyPsychologist, UpdatePsychologistProfile, ListPsychologistProfile
 
 urlpatterns = [
-    path('', ListCreatePsychologistProfile.as_view(), name='list-create-psychologist-profile'),
+    path('new/profile/', CreatePsychologistProfile.as_view(), name='create-psychologist-profile'),
     path('search/', SearchPsychologists.as_view(), name='serach-psychologists'),
     path('me/<int:user_id>/', UpdateDestroyPsychologist.as_view(), name='retrieve-update-destroy-psychologist'),
     path('profile/<int:user_id>/', UpdatePsychologistProfile.as_view(), name='update-psychologist'),
+    path('', ListPsychologistProfile.as_view(), name='list-all-psychologist-profile')
 ]
