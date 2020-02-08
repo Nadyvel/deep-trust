4b5f29f3868447e16641f5dcb01c0deba2f0db7f
hmanaog <hmanaog@gmail.com>
2020-02-08 17:54:31 +0100

    Initial commit of psychologist profile.

--- frontend/src/components/PsychologistProfile/PsychologistPatients/PatientList/PatientList.js
+++ frontend/src/components/PsychologistProfile/PsychologistPatients/PatientList/PatientList.js
@@ -7,7 +7,6 @@ const PatientList = (props) => {
             <h1>MY PATIENTS</h1>
             {props.patientCards && props.patientCards.map((patient, index) => {
                 return <div className="eachPatientProfile" key={index}>
-
                     <p className='patientName'>{patient.first_name} {patient.last_name} </p>
                     <div className='underLine'></div>
                     <p className='patientEmail'>Email: {patient.email}</p>
@@ -15,7 +14,6 @@ const PatientList = (props) => {
                     <p className='patientProblemDescription'>Patient info: {patient.problem_description}</p>
                     <p className='patientSpecialNotes'>Special notes: {patient.special_notes}</p>
                     <p className='patientTodosNotes'>Todo notes: {patient.todos}</p>
-
                 </div>
             })}
         </div>
