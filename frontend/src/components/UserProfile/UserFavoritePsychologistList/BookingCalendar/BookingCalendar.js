06c954353fed320d158f7f9a84c965a86fda4432
hmanaog <hmanaog@gmail.com>
2020-02-11 13:46:03 +0100

    initial commit for booking

--- frontend/src/components/UserProfile/UserFavoritePsychologistList/BookingCalendar/BookingCalendar.js
+++ frontend/src/components/UserProfile/UserFavoritePsychologistList/BookingCalendar/BookingCalendar.js
@@ -3,29 +3,30 @@ import './BookingCalendar.css'
 import 'react-calendar/dist/Calendar.css';
 
 import Calendar from './Calendar'
+//cursor: pointer
 
 const BookingCalendar = (props) => {
     return (
         <>
+        <div className="booking-psychologist-container">
+        <div className="information-contianer-wrapper">   
         <div className='booking-psychologist-name-warp'>
             <img src={props.location.psychologist.image} className="booking-psychologist-image" alt={`${props.location.psychologist.first_name} ${props.location.psychologist.last_name}`} />
         <p className='psychologistName'>{props.location.psychologist.first_name} {props.location.psychologist.last_name} </p>
     </div>
-        <div className="booking-psychologist-container">
-        
+        <div className="information-wrap">
         <div className='booking-psychologist-information-wrapper'>
             <p className='psychologistEmail'>Country: {props.location.psychologist.country}</p>
             <p className='psychologistBirthDate'>City: {props.location.psychologist.city}</p>
-            <p className='psychologistProblemDescription'>ZIP: {props.location.psychologist.zip}</p>
         </div>
 
         <div className='booking-psychologist-work-info-wrapper'>
             <p className='psychologistSpecialNotes'>Working hour: {props.location.psychologist.working_hours}</p>
             <p className='psychologistTodosNotes'>Price per hour: {props.location.psychologist.price_per_hour}</p>
         </div>
-
+        </div>
         <Calendar />
-
+        </div>
         </div>
         </>
     )
