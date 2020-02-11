c91c1ada2cb73cdd6d7a88d2b595b68ae694fb76
hmanaog <hmanaog@gmail.com>
2020-02-11 17:00:59 +0100

    initial commit of mybookings

--- frontend/src/components/TwilioVideo/VideoChat.js
+++ frontend/src/components/TwilioVideo/VideoChat.js
@@ -14,12 +14,28 @@ const VideoChat = (props) => {
 	const data = props.location.myBooking.user.username
 	console.log('username', data)
 
-	// useEffect(() => {
-	// 	setUsername({
-	// 		username:props.location.myBooking.user.username 
-	// 	})
-	// 	console.log('setUsername', username)
-	// },[props.location, setUsername, username])
+	const rName = props.location.myBooking.user.id + props.location.myBooking.user.email 
+	+ props.location.myBooking.id + props.location.myBooking.date + props.location.myBooking.time 
+	+ props.location.myBooking.psychologist
+	console.log('room', rName)
+
+	const roName = ''
+
+
+	useEffect(() => {
+		if(!username) {
+		setUsername({
+			username:props.location.myBooking.user.username 
+		})
+		setRoomName({
+			roomName: rName
+		})
+	}
+		console.log('setUsername', username)
+		console.log('roomName', roomName)
+	},[])
+
+	console.log('in da state', username)
 
 	const handleUsernameChange = useCallback(event => {
 		setUsername(event.currentTarget.value);
@@ -62,13 +78,40 @@ const VideoChat = (props) => {
 		);
 	} else {
 		render = (
-			<Lobby
-				username={username}
-				roomName={roomName}
-				handleUsernameChange={handleUsernameChange}
-				handleRoomNameChange={handleRoomNameChange}
-				handleSubmit={handleSubmit}
-			/>
+			// <Lobby
+			// 	username={username.username}
+			// 	roomName={roomName.roomName}
+			// 	// handleUsernameChange={handleUsernameChange}
+			// 	// handleRoomNameChange={handleRoomNameChange}
+			// 	handleSubmit={handleSubmit}
+			// />
+
+			<form onSubmit={handleSubmit}>
+      			<h2>Enter a room</h2>
+      		<div>
+        		<label htmlFor="name">Name:</label>
+        	<input
+          		type="text"
+          		id="field"
+          		value={username.username}
+          		onChange={handleUsernameChange}
+          		required
+        	/>
+      		</div>
+
+      		<div>
+        		<label htmlFor="room">Room name:</label>
+        	<input
+          		type="text"
+          		id="room"
+          		value={roomName}
+          		onChange={handleRoomNameChange}
+          		required
+        	/>
+      		</div>
+
+      		<button type="submit">Submit</button>
+    		</form>
 		);
 	}
 	return render;
