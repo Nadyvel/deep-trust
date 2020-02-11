e1a8dca6bc9cd3689dab8b17c225167989027901
Jessica Gibbons <jessica@Jessicas-MacBook-Air.local>
2020-02-11 14:40:48 +0100

    update doc data

--- frontend/src/components/PsychologistProfile/PsychologistSetting/PsychologistSetting.js
+++ frontend/src/components/PsychologistProfile/PsychologistSetting/PsychologistSetting.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
+import {psychologistUpdateProfile} from '../../../store/action/psychologistsAction';
 
 const PsychologistSetting = (props) => {
-   
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        const initialState = {
+            first_name: '',
+            last_name: '',
+            country: '',
+            city: '',
+            description: '',
+            working_hours: '',
+            price_per_hour: '',
+        };
+        setData(initialState);
+    }, []);
+
+    const handdleChange = e => {
+        const name = e.target.name;
+        const val = e.target.value;
+        setData({ ...data, [name]: { ...data[name], value: val } });
+    };
+
+    const handdleUpdate = async e => {
+        e.preventDefault();
+        const updateData = {};  // where I will store the new data
+        Object.keys(data).forEach(key => {
+            return ( !data[key] ? updateData[key] = props.myProfile.psychologists[key] : 
+            updateData[key] = data[key].value);
+        }); 
+     
+        try {
+            await props.dispatch(psychologistUpdateProfile(updateData));
+            props.history.push('psychologist/me');
+        } catch (err) {
+            // nothing for now
+        }
+    };
+
     return (
         
         <div className='user-settings'>
@@ -11,7 +48,7 @@ const PsychologistSetting = (props) => {
                 <h1 className='profile-update-title'>Update your profile</h1>
             </div>
         
-            <form className='update-form' >
+            <form className='update-form' onSubmit={handdleUpdate}>
 
                 {/* <div className='user-settings-image-styling'>
                     <div className='bold'>Image</div>
@@ -21,13 +58,13 @@ const PsychologistSetting = (props) => {
                 <div>
                     <label className='update-label'>First Name</label>
                     <input className='update-input' name="first_name" type='text'
-                    defaultValue={props.myProfile.psychologists.first_name}/>
+                    defaultValue={props.myProfile.psychologists.first_name} onChange={handdleChange}/>
                 </div>
 
                 <div>
                     <label className='update-label'>Last Name</label>
                     <input className='update-input' name="last_name" type='text'
-                    defaultValue={props.myProfile.psychologists.last_name} />
+                    defaultValue={props.myProfile.psychologists.last_name} onChange={handdleChange}/>
                 </div>
 
                 {/* <div>
@@ -40,13 +77,13 @@ const PsychologistSetting = (props) => {
                     <div>
                         <label className='update-label'>Country</label>
                         <input className='update-input-country' name="country" type='text'
-                        defaultValue={props.myProfile.psychologists.country}/>
+                        defaultValue={props.myProfile.psychologists.country} onChange={handdleChange}/>
                     </div>
 
                     <div>
                         <label className='update-label'>City</label>
                         <input className='update-input-city' name="city" type='text'
-                        defaultValue={props.myProfile.psychologists.city}/>
+                        defaultValue={props.myProfile.psychologists.city} onChange={handdleChange}/>
                     </div>
                 </div>
 
@@ -59,19 +96,19 @@ const PsychologistSetting = (props) => {
                 <div>
                     <label className='update-label'>Description</label>
                     <input className='update-input' name="description" type='text'
-                    defaultValue={props.myProfile.psychologists.description}/>
+                    defaultValue={props.myProfile.psychologists.description} onChange={handdleChange}/>
                 </div>
 
                 <div>
                     <label className='update-label'>Working Hours</label>
                     <input className='update-input' name="working_hours" type='text'
-                    defaultValue={props.myProfile.psychologists.working_hours}/>
+                    defaultValue={props.myProfile.psychologists.working_hours} onChange={handdleChange}/>
                 </div>
 
                 <div>
                     <label className='update-label'>Price per Hour</label>
                     <input className='update-input' name="price_per_hour" type='text'
-                    defaultValue={props.myProfile.psychologists.price_per_hour}/>
+                    defaultValue={props.myProfile.psychologists.price_per_hour} onChange={handdleChange}/>
                 </div>
 
                 <button className='save-button' type="submit" content="Save">Save</button>
