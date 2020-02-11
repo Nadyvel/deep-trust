685208becfc79f87bac09d84369f83829ed1d211
Jessica Gibbons <jessica@Jessicas-MacBook-Air.local>
2020-02-11 11:07:55 +0100

    style doctor settings

--- frontend/src/components/PsychologistProfile/PsychologistSetting/PsychologistSetting.js
+++ frontend/src/components/PsychologistProfile/PsychologistSetting/PsychologistSetting.js
@@ -1,66 +1,90 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import {withRouter} from 'react-router-dom'
+import React from 'react';
+import {connect} from 'react-redux';
+import {withRouter} from 'react-router-dom';
 
 const PsychologistSetting = (props) => {
-    console.log('setting props', props)
+   
     return (
-        <div className="settings_container">
-            <div className='user-settings-image-styling'>
-                <div className='bold'>Image</div>
-                {props.myProfile.psychologists.image}
-            </div>
+        
+        <div className='user-settings'>
             <div className='user-settings-title'>
-                {props.myProfile.username}'s Profile
+                <h1 className='profile-update-title'>Update your profile</h1>
             </div>
-            <div className='user-settings-styling'>
-                <div className='bold'>Email</div>
-                {props.myProfile.email}
-            </div>
-            <div className='user-settings-styling'>
-                <div className='bold'>Username</div>
-                {props.myProfile.username}
-            </div>
-            <div className='user-settings-styling'>
-                <div className='bold'>First name</div>
-                {props.myProfile.psychologists.first_name}
-            </div>
-            <div className='user-settings-styling'>
-                <div className='bold'>Last name</div>
-                {props.myProfile.psychologists.last_name}
-            </div>
-            <div className='user-settings-styling'>
-                <div className='bold'>Country</div>
-                {props.myProfile.psychologists.country}
-            </div>
-            <div className='user-settings-styling'>
-                <div className='bold'>City</div>
-                {props.myProfile.psychologists.city}
-            </div>
-            <div className='user-settings-styling'>
-                <div className='bold'>ZIP</div>
-                {props.myProfile.psychologists.zip}
-            </div>
-            <div className='user-settings-styling'>
-                <div className='bold'>Description</div>
-                {props.myProfile.psychologists.description}
-            </div>
-            <div className='user-settings-styling'>
-                <div className='bold'>Working Hour</div>
-                {props.myProfile.psychologists.working_hours}
-            </div>
-            <div className='user-settings-styling'>
-                <div className='bold'>Price per Hour</div>
-                {props.myProfile.psychologists.price_per_hour}
-            </div>
+        
+            <form className='update-form' >
+
+                {/* <div className='user-settings-image-styling'>
+                    <div className='bold'>Image</div>
+                    {props.myProfile.psychologists.image}
+                </div> */}
+            
+                <div>
+                    <label className='update-label'>First Name</label>
+                    <input className='update-input' name="first_name" type='text'
+                    defaultValue={props.myProfile.psychologists.first_name}/>
+                </div>
+
+                <div>
+                    <label className='update-label'>Last Name</label>
+                    <input className='update-input' name="last_name" type='text'
+                    defaultValue={props.myProfile.psychologists.last_name} />
+                </div>
+
+                {/* <div>
+                    <label className='update-label'>Username</label>
+                    <input className='update-input' name="username" type='text'
+                    defaultValue={props.myProfile.username} />
+                </div> */}
+
+                <div className='LocationData'>
+                    <div>
+                        <label className='update-label'>Country</label>
+                        <input className='update-input-country' name="country" type='text'
+                        defaultValue={props.myProfile.psychologists.country}/>
+                    </div>
+
+                    <div>
+                        <label className='update-label'>City</label>
+                        <input className='update-input-city' name="city" type='text'
+                        defaultValue={props.myProfile.psychologists.city}/>
+                    </div>
+                </div>
+
+                {/* <div>
+                    <label className='update-label'>ZIP</label>
+                    <input className='update-input' name="zip" type='text'
+                    defaultValue={props.myProfile.psychologists.zip}/>
+                </div> */}
+
+                <div>
+                    <label className='update-label'>Description</label>
+                    <input className='update-input' name="description" type='text'
+                    defaultValue={props.myProfile.psychologists.description}/>
+                </div>
+
+                <div>
+                    <label className='update-label'>Working Hours</label>
+                    <input className='update-input' name="working_hours" type='text'
+                    defaultValue={props.myProfile.psychologists.working_hours}/>
+                </div>
+
+                <div>
+                    <label className='update-label'>Price per Hour</label>
+                    <input className='update-input' name="price_per_hour" type='text'
+                    defaultValue={props.myProfile.psychologists.price_per_hour}/>
+                </div>
+
+                <button className='save-button' type="submit" content="Save">Save</button>
+
+            </form>
         </div>
-    )
-}
+    );
+};
 
 const mapStateToProps = (state, props) => {
     return{
         myProfile: state.psychologistsReducer.myProfile
-    }
-}
+    };
+};
 
-export default connect(mapStateToProps)(withRouter(PsychologistSetting))
+export default connect(mapStateToProps)(withRouter(PsychologistSetting));
