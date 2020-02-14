a4359d72b339e1ccbce700a959969c868f2b4b0c
Nadja Gujer <velnady@gmail.com>
2020-02-14 15:03:25 +0100

    user card video path

--- frontend/src/components/UserProfile/UserCard/UserCard.js
+++ frontend/src/components/UserProfile/UserCard/UserCard.js
@@ -55,7 +55,7 @@ const UserCard = (props) => {
                 <Route exact path='/userprofile/settings' component={UserSettings}/>
                 <Route path='/userprofile/psychologist' component={BookingCalendar}/>
                 <Route exact path='/userprofile/myBookings' component={MyBookings} />
-                <Route exact path='/userprofile/myBookings/details' component={MyBookingDetails} />
+                <Route path='/userprofile/myBookings/details' component={MyBookingDetails} />
                 <Route exact path='/userprofile/emergency' component={Emergency}/>
                 <DeleteUserModal />
             </div>
