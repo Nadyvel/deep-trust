b31cd27319a3741d58c4b8e966a2254eac4abacf
hmanaog <hmanaog@gmail.com>
2020-02-14 12:31:36 +0100

    Edited Css for booking details

--- frontend/src/components/UserProfile/MyBookings/MyBookingDetails/MyBookingDetails.js
+++ frontend/src/components/UserProfile/MyBookings/MyBookingDetails/MyBookingDetails.js
@@ -39,13 +39,15 @@ const MyBookingDetails = (props) => {
                 <p className='myBooking-detail'>Time of Meeting:</p>
             </div>
             <div className="myBooking-psychologist-detail-wrap">
-                <p>{props.location.myBooking.psychologist_first_name} {props.location.myBooking.psychologist_last_name}</p>
-                <p>{props.location.myBooking.date}</p>
-                <p>{props.location.myBooking.time_in_str}</p>
+                <p className="details-info-psycho-wrap">{props.location.myBooking.psychologist_first_name} {props.location.myBooking.psychologist_last_name}</p>
+                <p className="details-info-psycho-wrap">{props.location.myBooking.date}</p>
+                <p className="details-info-psycho-wrap">{props.location.myBooking.time_in_str}</p>
             </div>
 
             <div className="video-chat-container">
-                <img className="video-logo" src={videoLogo} alt="video-logo" />
+                <div className="video-icon-wrapper">
+                <img className="video-icon" src={videoLogo} alt="video-logo" />
+                </div>
                 <Link className="videoLink" to={{pathname: '/video', data: props.location.myBooking, is_patient: true}} style={{textDecoration: 'none', pointerEvents: 'auto' }}>
                     Start Video Call
                 </Link>
